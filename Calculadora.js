a1ff75616decb847ed4ba804e33ceee50b4fa0ca
Facundo Vallejos <Facund097dv@gmail.com>
2023-08-17 17:34:51 -0300

    calculadora

---
+++ Calculadora.js
@@ -0,0 +1,10 @@
+// let $botonNumero = document.getElementById("boton");
+
+// $botonNumero.addEventListener('click',(event) =>{
+//     console.log("hiciste click xd");
+//     let element = event.target.attributes[0].value;
+    
+//     console.log(Number(element));
+    
+// });
+
