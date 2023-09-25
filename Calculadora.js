6cca0dc00d516781fa9f081e1258548d5b42738d
Facundo Vallejos <Facund097dv@gmail.com>
2023-09-25 17:50:23 -0300

    cambio de nombre del html

--- Calculadora.js
+++ Calculadora.js
@@ -1,10 +1,109 @@
-// let $botonNumero = document.getElementById("boton");
+/**
+ * Con este codigo obtengo el valor de cualquier boton
+ */
+let $botonNumero = document.getElementById("boton");
 
-// $botonNumero.addEventListener('click',(event) =>{
-//     console.log("hiciste click xd");
-//     let element = event.target.attributes[0].value;
-    
-//     console.log(Number(element));
-    
-// });
+$botonNumero.addEventListener("click", (event) => {
+  let element = event.target.textContent;
+  console.log(element);
+  insertarEnPantalla(element);
 
+  return element;
+});
+
+/**
+ * Modifica los valores de la pantalla
+ */
+
+let $inputPantalla = document.getElementById("inputPantalla");
+
+function insertarEnPantalla(valor) {
+  if (limiteCaracteresPantalla()) {
+    $inputPantalla.textContent += valor;
+  }
+  console.log($inputPantalla.textContent.length);
+}
+function limiteCaracteresPantalla() {
+  return $inputPantalla.textContent.length < 15;
+}
+
+var primes = [
+  2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71,
+  73, 79, 83, 89, 97,
+];
+
+function buscar(array, targetValue) {
+  var min = 0;
+  var max = array.length - 1;
+  var guess;
+
+  while (array[guess] !== targetValue) {
+    guess = Math.floor((max + min) / 2);
+    if (max < min) {
+      return -1;
+    } else if (array[guess] > targetValue) {
+      max = guess - 1;
+    } else {
+      min = guess + 1;
+    }
+  }
+
+  return guess;
+}
+const pass = "8c23e8c59";
+const email = "vdfacu97@gmail.com";
+function solicitud() {
+  const credentials = {
+    email: email,
+    password: pass
+  };
+
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(credentials),
+  };
+
+  fetch("http://dogs.magnet.cl/api/v1/auth/", options)
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error("No se pudo autenticar. Verifica tus credenciales.");
+      }
+    })
+    .then((data) => {
+      const token = data.token;
+      console.log("Token JWT:", token);
+    })
+    .catch((error) => {
+      console.error("Error:", error.message);
+    });
+
+  // const options = {
+  //   method:'POST',
+  //   headers: {
+  //     "Content-Type": "application/json"
+  //   },
+  //   body: JSON.stringify({
+  //     "email": email,
+  //     "password": pass
+  //   })
+  // }
+
+  // fetch("http://dogs.magnet.cl/api/v1/auth/", options)
+  // .then(response => {
+  //   if (response.ok)
+  //     return response.text();
+
+  //   throw new Error(response.status);
+  // })
+  // .then(data => {
+  //   console.log("Datos: " + data);
+  // })
+  // .catch(err => {
+  //   console.error("ERROR: ", err.message)
+  // });
+}
